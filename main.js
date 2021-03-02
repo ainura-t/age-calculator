2d7f229a98b2c648b926e354a67600383a984698
ainura-t <stredeva@gmail.com>
2021-03-02 16:55:54 -0500

    added calculator file

---
+++ main.js
@@ -0,0 +1,6 @@
+var current_year = 2021;
+var manas_birth = 2009;
+var manas_age = current_year - manas_birth;
+var either_age = manas_age - 1;
+
+document.write("He is either " + manas_age + " or " + either_age)
